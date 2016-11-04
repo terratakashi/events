7fa11024d60832171ecd607c639d080f8ee0dc3a
Alex Chen <alex@spoonrocket.com>
2016-11-04 01:57:55 -0700

    added new action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,8 @@ class EventsController < ApplicationController
   def index
 
   end
+
+  def new
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
 
-  resources :events, only: [:index]
+  resources :events, only: [:index, :new]
 end
