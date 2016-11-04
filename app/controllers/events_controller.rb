8fdab4e1a01b4ca0d7dc2f7d87dfb964c1c7e777
Alex Chen <alex@spoonrocket.com>
2016-11-04 02:56:19 -0700

    added create_events action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,27 @@ class EventsController < ApplicationController
   def new
 
   end
+
+  def create_events
+    events_json = JSON.parse(secure_params["json_body"])
+    if events_json.has_key?("events")
+      events_json["events"].each do |event|
+        add_event(event)
+      end
+    end
+
+    redirect_to events_path
+  end
+
+  private
+
+  def add_event(event)
+    event_date = DateTime.new(event["year"], event["month"], event["day"])
+    cancelled_status = event["cancelled"].present? ? event["cancelled"] : false
+    Event.create(occasion: event["occasion"], date: event_date, invited_count: event["invited_count"], cancelled: cancelled_status)
+  end
+
+  def secure_params
+    params.permit(:json_body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
   resources :events, only: [:index, :new]
+
+  post "/events_json", to: "events#create_events"
 end
